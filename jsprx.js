b28526f721084a2315a0f3a38ca50043d083aa09
Demenyaca <demenyaca@gmail.com>
2019-02-27 16:14:55 -0500

    Table  needed

--- jsprx.js
+++ jsprx.js
@@ -1,1 +1,7 @@
-alert("HuuuuumpDay!");
+var kyle = prompt("How old is Kyle?");
+var son  =document.getElementById("Kyle");
+son.innerHTML = kyle;
+
+var car = prompt ("Which car do you drive?");
+var brand = docuemnt.getElementById("car");
+brand.innerHTML = car;
